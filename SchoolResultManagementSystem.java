78bef96cf035fdffce6f909076eb73d32a54ba60
TrueAzmiAsif <100873454+TrueAzmiAsif@users.noreply.github.com>
2022-11-28 12:32:29 -0800

    Add files via upload

---
+++ SchoolResultManagementSystem.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package school.result.management.system;
+
+/**
+ *
+ * @author admin
+ */
+public class SchoolResultManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Index.setVisible(true);
+    }
+    
+}
